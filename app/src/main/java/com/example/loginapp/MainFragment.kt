88d38e7207ccdce28aef001c73a17402d3385dfb
Octavio Resendiz Bolaños <octavioresendiz36@aragon.unam.mx>
2022-10-07 15:35:34 -0500

    first commit

---
+++ app/src/main/java/com/example/loginapp/MainFragment.kt
@@ -0,0 +1,14 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/loginapp/SignUpFragment.kt
@@ -0,0 +1,14 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
